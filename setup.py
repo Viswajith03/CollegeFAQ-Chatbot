9e78fb4c61c8423ccd1594620c7e5f2bc6e3b3e2
Viswajith03 <viswajith.v.nair@gmail.com>
2025-01-19 10:13:44 +0530

    Folder Structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Viswajith Viswanathan',
+    author_email= 'viswajith.v.nair@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
